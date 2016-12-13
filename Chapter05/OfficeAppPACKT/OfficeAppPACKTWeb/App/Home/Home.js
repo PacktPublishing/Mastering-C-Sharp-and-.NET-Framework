3b56526220617e5fb9b51d6dd41add6636c226ec
bhavinsavalia18 <bhavinsavalia@packtpub.com>
2016-12-13 15:24:59 +0530

    Code files addded

---
+++ Chapter05/OfficeAppPACKT/OfficeAppPACKTWeb/App/Home/Home.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+            $('#get-data-from-selection').click(getDataFromSelection);
+            $('#insert-data-from-Task-Pane').click(insertDataFromTaskPane);
+        });
+    };
+
+    // Reads data from current document selection and displays a notification
+    function getDataFromSelection() {
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    app.showNotification('The selected text is:', '"' + result.value + '"');
+                } else {
+                    app.showNotification('Error:', result.error.message);
+                }
+            }
+        );
+    }
+    function insertDataFromTaskPane() {
+        Office.context.document.setSelectedDataAsync(
+                'Text inserted from the Task Pane' +
+                '\nCallaback function notifies status',
+                function (result) {
+                app.showNotification('Feedback:', result.status);
+            });
+    }
+
+})();
